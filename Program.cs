930b83e0916f752348ad4d0a468f52f43d077450
shahdayman315315 <shahdayman315315@gmail.com>
2025-08-22 00:20:42 +0300

    Add Docs

--- Program.cs
+++ Program.cs
@@ -19,6 +19,9 @@ namespace Rezk_Proj
             // Add services to the container.
 
             builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+           
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
             builder.Services.Configure<JWT>(builder.Configuration.GetSection("JWT"));
@@ -54,10 +57,11 @@ namespace Rezk_Proj
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
+           
+                app.UseSwagger();
+                app.UseSwaggerUI();
                 app.MapOpenApi();
-            }
+            
 
             app.UseHttpsRedirection();
 
